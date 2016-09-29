9aebb61058b9f3a9dd61e06d9af845f4171b6360
TamarCohen <tcohen@mit.edu>
2016-09-29 15:45:49 -0400

    Missing fig

--- code/P1/P1.py
+++ code/P1/P1.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
                                                                              [-70, 80], StepSize, Error,
                                                                              0, 0, 100000, gaussMean, gaussCov)
     # print "Gaussian - Function surface + path - Gaussian"
-    PlotGraph.PlotGraph3D("Gaussian Function and the Gradient Descent Path", Functions.GaussianFunc, GaussGradPath,
-                          gaussMean, gaussCov)
+    # PlotGraph.PlotGraph3D("Gaussian Function and the Gradient Descent Path", Functions.GaussianFunc, GaussGradPath,
+    #                       gaussMean, gaussCov)
 
     # print "Gaussian - convergence of the gradient"
     xes = range(0, len(GradNormGauss))
@@ -38,26 +38,26 @@ if __name__ == '__main__':
     plt.xlabel('Iteration')
     plt.ylabel('Second Norm of the Gradient')
     plt.title('Gradient Norm Convergence rate - Gaussian')
-
-    # print "Gaussian - convergence - different points"
-    ConvergenceRate = np.zeros((10, 10))
-    for x in range(0, 10):
-        for y in range(0, 10):
-            GaussGradPath, GaussOpt, GradNorm = GradientDescent.GradientDescent(Functions.GaussianFunc,
-                                                                                Functions.GradientGaussianFunc,
-                                                                                [x, y], StepSize,
-                                                                                Error, 0, 0, 100000, gaussMean,
-                                                                                gaussCov)
-            ConvergenceRate[x, y] = len(GradNorm)
-    plt.figure(figsize=(15, 10))
-    plt.subplot()
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('Convergence for Different Starting Points - Gaussian')
-    plt.pcolor(ConvergenceRate)
-    plt.axis([0, 10, 0, 10])
-    plt.colorbar()
-    plt.savefig('Convergence for Different Starting Points - Gaussian.png')
+    plt.savefig('Gradient Norm Convergence rate - Gaussian.png')
+    # # print "Gaussian - convergence - different points"
+    # ConvergenceRate = np.zeros((10, 10))
+    # for x in range(0, 10):
+    #     for y in range(0, 10):
+    #         GaussGradPath, GaussOpt, GradNorm = GradientDescent.GradientDescent(Functions.GaussianFunc,
+    #                                                                             Functions.GradientGaussianFunc,
+    #                                                                             [x, y], StepSize,
+    #                                                                             Error, 0, 0, 100000, gaussMean,
+    #                                                                             gaussCov)
+    #         ConvergenceRate[x, y] = len(GradNorm)
+    # plt.figure(figsize=(15, 10))
+    # plt.subplot()
+    # plt.xlabel('x')
+    # plt.ylabel('y')
+    # plt.title('Convergence for Different Starting Points - Gaussian')
+    # plt.pcolor(ConvergenceRate)
+    # plt.axis([0, 10, 0, 10])
+    # plt.colorbar()
+    # plt.savefig('Convergence for Different Starting Points - Gaussian.png')
 
     # # print "Gaussian - convergence - step size"
     # ConvergenceRate = []
@@ -96,72 +96,72 @@ if __name__ == '__main__':
     # plt.title('Convergence as a Function of the Precision Level - Gaussian function')
 
     #print "Gaussian - convergence - error and step size"
-    ConvergenceRate = np.zeros((len(Errors), len(StepSizes)))
-    for Error in range(0, len(Errors)):
-        for StepSize in range(0, len(StepSizes)):
-            print Error, ",", StepSize, ",", time.time()
-            GaussGradPath, GaussOpt, GradNorm = GradientDescent.GradientDescent(Functions.GaussianFunc,
-                                                                                Functions.GradientGaussianFunc,
-                                                                                [-10, 10],
-                                                                                math.exp(StepSizes[StepSize]),
-                                                                                math.exp(Errors[Error]), 0, 0, 1000000,
-                                                                                gaussMean, gaussCov)
-            ConvergenceRate[Error, StepSize] = len(GradNorm)
-    plt.figure(figsize=(15, 10))
-    plt.subplot()
-    plt.xlabel('ln(Precision Level)')
-    plt.ylabel('ln(Learning Rate)')
-    plt.title('Convergence as a Function of the Precision Level and the Learning Rate - Gaussian')
-    plt.xticks(range(0, len(StepSizes)), StepSizes)
-    plt.yticks(range(0, len(Errors)), Errors)
-    plt.pcolor(ConvergenceRate)
-    print ConvergenceRate
-    plt.colorbar()
-    plt.savefig('Convergence as a Function of the Precision Level and the Learning Rate - Gaussian.png')
+    # ConvergenceRate = np.zeros((len(Errors), len(StepSizes)))
+    # for Error in range(0, len(Errors)):
+    #     for StepSize in range(0, len(StepSizes)):
+    #         print Error, ",", StepSize, ",", time.time()
+    #         GaussGradPath, GaussOpt, GradNorm = GradientDescent.GradientDescent(Functions.GaussianFunc,
+    #                                                                             Functions.GradientGaussianFunc,
+    #                                                                             [-10, 10],
+    #                                                                             math.exp(StepSizes[StepSize]),
+    #                                                                             math.exp(Errors[Error]), 0, 0, 1000000,
+    #                                                                             gaussMean, gaussCov)
+    #         ConvergenceRate[Error, StepSize] = len(GradNorm)
+    # plt.figure(figsize=(15, 10))
+    # plt.subplot()
+    # plt.xlabel('ln(Precision Level)')
+    # plt.ylabel('ln(Learning Rate)')
+    # plt.title('Convergence as a Function of the Precision Level and the Learning Rate - Gaussian')
+    # plt.xticks(range(0, len(StepSizes)), StepSizes)
+    # plt.yticks(range(0, len(Errors)), Errors)
+    # plt.pcolor(ConvergenceRate)
+    # print ConvergenceRate
+    # plt.colorbar()
+    # plt.savefig('Convergence as a Function of the Precision Level and the Learning Rate - Gaussian.png')
 
 
-
-    print "Q1.1 - Quadratic Bowl"
-    Errors = range(-25, -1)
-    StepSizes = range(-13, -3)
-    StepSize = 0.1
-    Error = 0.00001
-    GaussGradPath, GaussOpt, GradNormBowl = GradientDescent.GradientDescent(Functions.QuadraticBowlFunc,
-                                                                            Functions.GradientQuadraticBowlFunc,
-                                                                            [-70, 80], StepSize, Error,
-                                                                            0, 0, 100000, quadBowlA, quadBowlb)
+    #
+    # print "Q1.1 - Quadratic Bowl"
+    # Errors = range(-25, -1)
+    # StepSizes = range(-13, -3)
+    # StepSize = 0.1
+    # Error = 0.00001
+    # GaussGradPath, GaussOpt, GradNormBowl = GradientDescent.GradientDescent(Functions.QuadraticBowlFunc,
+    #                                                                         Functions.GradientQuadraticBowlFunc,
+    #                                                                         [-70, 80], StepSize, Error,
+    #                                                                         0, 0, 100000, quadBowlA, quadBowlb)
     # print "Quadratic Bowl - Function surface + path"
-    PlotGraph.PlotGraph3D("Quadratic Bowl Function and the Gradient Descent Path", Functions.QuadraticBowlFunc,
-                          GaussGradPath, quadBowlA, quadBowlb)
-
-    # print "Quadratic Bowl - convergence of the gradient"
-    xes = range(0, len(GradNormBowl))
-    plt.figure(figsize=(15, 10))
-    plt.subplot()
-    plt.plot(xes, GradNormBowl)
-    plt.xlabel('Iteration')
-    plt.ylabel('Second Norm of the Gradient')
-    plt.title('Gradient Norm Convergence rate - Quadratic Bowl')
-    plt.savefig('Gradient Norm Convergence rate - Quadratic Bowl.png')
-
-    # print "Quadratic Bowl - convergence - different points"
-    ConvergenceRate = np.zeros((10, 10))
-    for x in range(0, 10):
-        for y in range(0, 10):
-            GaussGradPath, GaussOpt, GradNorm = GradientDescent.GradientDescent(Functions.QuadraticBowlFunc,
-                                                                                Functions.GradientQuadraticBowlFunc,
-                                                                                [x, y], StepSize, Error,
-                                                                                0, 0, 100000, quadBowlA, quadBowlb)
-            ConvergenceRate[x, y] = len(GradNorm)
-    plt.figure(figsize=(15, 10))
-    plt.subplot()
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('Convergence for Different Starting Points - Quadratic Bowl')
-    plt.pcolor(ConvergenceRate)
-    plt.axis([0, 10, 0, 10])
-    plt.colorbar()
-    plt.savefig('Convergence for Different Starting Points - Quadratic Bowl.png')
+    # PlotGraph.PlotGraph3D("Quadratic Bowl Function and the Gradient Descent Path", Functions.QuadraticBowlFunc,
+    #                       GaussGradPath, quadBowlA, quadBowlb)
+    #
+    # # print "Quadratic Bowl - convergence of the gradient"
+    # xes = range(0, len(GradNormBowl))
+    # plt.figure(figsize=(15, 10))
+    # plt.subplot()
+    # plt.plot(xes, GradNormBowl)
+    # plt.xlabel('Iteration')
+    # plt.ylabel('Second Norm of the Gradient')
+    # plt.title('Gradient Norm Convergence rate - Quadratic Bowl')
+    # plt.savefig('Gradient Norm Convergence rate - Quadratic Bowl.png')
+    #
+    # # print "Quadratic Bowl - convergence - different points"
+    # ConvergenceRate = np.zeros((10, 10))
+    # for x in range(0, 10):
+    #     for y in range(0, 10):
+    #         GaussGradPath, GaussOpt, GradNorm = GradientDescent.GradientDescent(Functions.QuadraticBowlFunc,
+    #                                                                             Functions.GradientQuadraticBowlFunc,
+    #                                                                             [x, y], StepSize, Error,
+    #                                                                             0, 0, 100000, quadBowlA, quadBowlb)
+    #         ConvergenceRate[x, y] = len(GradNorm)
+    # plt.figure(figsize=(15, 10))
+    # plt.subplot()
+    # plt.xlabel('x')
+    # plt.ylabel('y')
+    # plt.title('Convergence for Different Starting Points - Quadratic Bowl')
+    # plt.pcolor(ConvergenceRate)
+    # plt.axis([0, 10, 0, 10])
+    # plt.colorbar()
+    # plt.savefig('Convergence for Different Starting Points - Quadratic Bowl.png')
 
     # # print "Quadratic Bowl - convergence - step size"
     # ConvergenceRate = []
@@ -197,55 +197,55 @@ if __name__ == '__main__':
     # plt.ylabel('Num of iterations to convergence')
     # plt.title('Convergence as a Function of the Precision Level - Quadratic Bowl')
 
-    # print "Gaussian - convergence - error and step size"
-    ConvergenceRate = np.zeros((len(Errors), len(StepSizes)))
-    for Error in range(0, len(Errors)):
-        for StepSize in range(0, len(StepSizes)):
-            print Error, ",", StepSize, ",", time.time()
-            GaussGradPath, GaussOpt, GradNorm = GradientDescent.GradientDescent(Functions.QuadraticBowlFunc,
-                                                                                Functions.GradientQuadraticBowlFunc,
-                                                                                [-10, 10],
-                                                                                math.exp(StepSizes[StepSize]),
-                                                                                math.exp(Errors[Error]), 0, 0, 1000000,
-                                                                                quadBowlA, quadBowlb)
-            ConvergenceRate[Error, StepSize] = len(GradNorm)
-    plt.figure(figsize=(15, 10))
-    plt.subplot()
-    plt.xlabel('ln(Precision Level)')
-    plt.ylabel('ln(Learning Rate)')
-    plt.title('Convergence as a Function of the Precision Level and the Learning Rate - Quadratic Bowl')
-    plt.xticks(range(0, len(StepSizes)), StepSizes)
-    plt.yticks(range(0, len(Errors)), Errors)
-    plt.pcolor(ConvergenceRate)
-    print ConvergenceRate
-    plt.colorbar()
-    plt.savefig('Convergence as a Function of the Precision Level and the Learning Rate - Quadratic Bowl.png')
-
+    # # print "Gaussian - convergence - error and step size"
+    # ConvergenceRate = np.zeros((len(Errors), len(StepSizes)))
+    # for Error in range(0, len(Errors)):
+    #     for StepSize in range(0, len(StepSizes)):
+    #         print Error, ",", StepSize, ",", time.time()
+    #         GaussGradPath, GaussOpt, GradNorm = GradientDescent.GradientDescent(Functions.QuadraticBowlFunc,
+    #                                                                             Functions.GradientQuadraticBowlFunc,
+    #                                                                             [-10, 10],
+    #                                                                             math.exp(StepSizes[StepSize]),
+    #                                                                             math.exp(Errors[Error]), 0, 0, 1000000,
+    #                                                                             quadBowlA, quadBowlb)
+    #         ConvergenceRate[Error, StepSize] = len(GradNorm)
+    # plt.figure(figsize=(15, 10))
+    # plt.subplot()
+    # plt.xlabel('ln(Precision Level)')
+    # plt.ylabel('ln(Learning Rate)')
+    # plt.title('Convergence as a Function of the Precision Level and the Learning Rate - Quadratic Bowl')
+    # plt.xticks(range(0, len(StepSizes)), StepSizes)
+    # plt.yticks(range(0, len(Errors)), Errors)
+    # plt.pcolor(ConvergenceRate)
+    # print ConvergenceRate
+    # plt.colorbar()
+    # plt.savefig('Convergence as a Function of the Precision Level and the Learning Rate - Quadratic Bowl.png')
     #
-    #
-    #
-    #
-    # # ##PART 2:
-    # # ##=======
-    x = y = np.arange(-100, 100, 0.1)
-    GradApprox = []
-    for i in range(0, 1000000):
-        SampledPointX = random.randint(0, len(x)-1)
-        SampledPointY = random.randint(0, len(x)-1)
-        Err = Functions.GradientGaussianFunc([x[SampledPointX], y[SampledPointY]], gaussMean, gaussCov) - \
-                          FiniteDifferenceApproximation.FiniteDifferenceApproximation\
-                          ([x[SampledPointX], y[SampledPointY]], 0.1, Functions.GaussianFunc, gaussMean, gaussCov)
-        GradApprox.append(math.sqrt(np.dot(np.array(Err).T, np.array(Err))))
-    counts, bin_edges = np.histogram(GradApprox, bins=100, normed=True)
-    cdf = np.cumsum(counts)
-    plt.plot(bin_edges[1:], cdf)
-    plt.xlabel('Squared Error')
-    plt.ylabel('Cumulative Amount')
-    plt.title('Cumulative Gradient Error')
-    plt.savefig('Cumulative Gradient Error.png')
     # #
     # #
     # #
+    # #
+    # # # ##PART 2:
+    # # # ##=======
+    # x = y = np.arange(-100, 100, 0.1)
+    # GradApprox = []
+    # for i in range(0, 1000000):
+    #     SampledPointX = random.randint(0, len(x)-1)
+    #     SampledPointY = random.randint(0, len(x)-1)
+    #     Err = Functions.GradientGaussianFunc([x[SampledPointX], y[SampledPointY]], gaussMean, gaussCov) - \
+    #                       FiniteDifferenceApproximation.FiniteDifferenceApproximation\
+    #                       ([x[SampledPointX], y[SampledPointY]], 0.1, Functions.GaussianFunc, gaussMean, gaussCov)
+    #     GradApprox.append(math.sqrt(np.dot(np.array(Err).T, np.array(Err))))
+    # counts, bin_edges = np.histogram(GradApprox, bins=100, normed=True)
+    # cdf = np.cumsum(counts)
+    # plt.plot(bin_edges[1:], cdf)
+    # plt.xlabel('Squared Error')
+    # plt.ylabel('Cumulative Amount')
+    # plt.title('Cumulative Gradient Error')
+    # plt.savefig('Cumulative Gradient Error.png')
+    # # #
+    # # #
+    # # #
 
     # ##PART 3:
     # ##=======
